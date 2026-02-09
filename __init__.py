d342a09fba6a04dff1d8cf99bcc8b701691a3fd0
efuentesamin <efuentesamin@gmail.com>
2026-02-09 09:09:32 -0500

    First commit

---
+++ __init__.py
@@ -0,0 +1,16 @@
+"""Top-level package for mewins_node_pack."""
+
+__all__ = [
+    "NODE_CLASS_MAPPINGS",
+    "NODE_DISPLAY_NAME_MAPPINGS",
+    
+]
+
+__author__ = """Edwin Fuentes"""
+__email__ = "efuentesamin@gmail.com"
+__version__ = "0.0.1"
+
+from .src.mewins_node_pack.nodes import NODE_CLASS_MAPPINGS
+from .src.mewins_node_pack.nodes import NODE_DISPLAY_NAME_MAPPINGS
+
+
---
+++ tests/__init__.py
@@ -0,0 +1,1 @@
+"""Unit test package for mewins_node_pack."""
